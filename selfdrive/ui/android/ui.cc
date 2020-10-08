6a0ac5e49a8a658b49fb45158776d2d6b18921ea
Administrator <beveltech@naver.com>
2020-10-08 19:14:03 +0900

    밝기조절삭제

--- selfdrive/ui/android/ui.cc
+++ selfdrive/ui/android/ui.cc
@@ -74,8 +74,6 @@ fail:
 }
 
 static void ui_set_brightness(UIState *s, int brightness) {
-  int perc = 80;
-  brightness = brightness * perc / 100;
   static int last_brightness = -1;
   if (last_brightness != brightness && (s->awake || brightness == 0)) {
     if (set_brightness(brightness)) {
